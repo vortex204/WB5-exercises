9719a162154917f2619484a3d079b2c8bbb4e445
vortex204 <jespinal1193@gmail.com>
2023-05-18 16:30:08 -0400

    partial dialog

---
+++ DynamicImages/scripts/data.js
@@ -0,0 +1,19 @@
+const images = [
+  {
+    year: 2023,
+    name: "FND_poster_LittleMermaid_PreSale.jpg",
+    title: "Little Mermaid",
+  },
+  {
+    year: 2023,
+    name: "FND_poster_Spider-ManATSV-Presale.jpg",
+    title: "Spider Man",
+  },
+  {
+    year: 2023,
+    name: "FND_poster_TranformersROTB-Presale.jpg",
+    title: "Transformers",
+  },
+  { year: 2023, name: "FSTX_Montage1Sheet2_RGB_1.jpg", title: "Montage" },
+  { year: 2024, name: "GuardiansVol3-2023.jpg", title: "Guardians Volume 3" },
+];
---
+++ DynamicImages/scripts/main.js
@@ -0,0 +1,67 @@
+// window.onload = ()=>{
+function showImage(path) {
+  const dialogImage = document.querySelector(".dialog .gigantic");
+  dialogImage.src = path;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const tbody = document.querySelector("#list tbody");
+
+  const year = document.getElementById("year");
+  const image_list = document.getElementById("image_list");
+  const output = document.getElementById("output");
+  const clear = document.getElementById("clear");
+
+  image_list.innerHTML = "<option>Make a selection</option>";
+
+  images.forEach((image) => {
+    let option = document.createElement("option");
+    option.innerHTML = image.title;
+    option.value = image.name;
+    image_list.appendChild(option);
+  }); // end images for each
+
+  function addMovieImageRow(image) {
+    let row = tbody.insertRow(-1); // end of rows
+
+    let cellYear = row.insertCell(0);
+    cellYear.innerHTML = image.year;
+
+    let cellTitle = row.insertCell(1);
+    cellTitle.innerHTML = image.title;
+
+    let fullPath = "DynamicImages/" + image.name;
+
+    let cellThumb = row.insertCell(2);
+    cellThumb.innerHTML = `<img src="${fullPath}" class="thumb" onclick="showImage('${fullPath}')">`;
+    // cellThumb.innerHTML = `
+    // <a href="${ fullPath }" target="_blank">
+    //     <img src="${ fullPath }" class="thumb">
+    // </a>
+    // `;
+  }
+
+  year.addEventListener("change", () => {
+    tbody.innerHTML = ""; //clear table body result rows
+    let selectedYear = year.selectedOptions[0].value;
+    images.filter((i) => i.year == selectedYear).forEach(addMovieImageRow);
+  }); // end year change listener
+
+  image_list.addEventListener("change", () => {
+    //IS FIRST ITEM "make a selection" e.g. 0, or no items e.g. -1
+    if (image_list.selectedIndex <= 0) {
+      return; // DO NOTHING
+    }
+
+    let name = image_list.selectedOptions[0].value;
+    // alert(name);
+    let img = document.createElement("img");
+    img.src = "DynamicImages/" + name;
+    output.appendChild(img);
+  }); // end on change image list
+
+  clear.addEventListener("click", () => {
+    output.innerHTML = "";
+    image_list.selectedIndex = 0; // set select to first item, -1 for blank
+  }); // end clear button click
+}); // document loaded
